541ff3d94d90befa9c5d3cd1ca183f9574089e8d
Ilja Buran <ilja.buran03@gmail.com>
2026-06-08 20:09:49 +0200

    Engine: started Delegates implementation

---
+++ Engine/Source/Common/Delegate.h
@@ -0,0 +1,122 @@
+#pragma once
+
+#include "Common.h"
+
+namespace Illulu
+{
+    class DelegateHandle
+    {
+    public:
+
+        constexpr DelegateHandle() noexcept;
+
+        [[nodiscard]] static DelegateHandle Create() noexcept
+        {
+            DelegateHandle handle;
+            handle.m_id = m_nextId++;
+            return handle;
+        }
+        
+        [[nodiscard]] bool IsValid() const noexcept
+        {
+            m_id != INVALID_ID;
+        }
+
+        void Reset() noexcept
+        {
+            m_id = INVALID_ID;
+        }
+
+    private:
+    
+        constexpr static u64 INVALID_ID = 0;
+    
+    private:
+        
+        u64 m_id = INVALID_ID;
+        inline static u64 m_nextId = 1;
+    };
+
+    template<typename... Args>
+    class RawDelegate
+    {
+        using Stub = void(*)(void* object, Args... args);
+
+    public:
+
+        constexpr RawDelegate() noexcept = default;
+
+        [[nodiscard]] IsBound() const noexcept
+        {
+            return m_stub != nullptr;
+        }
+
+        [[nodiscard]] IsBoundTo(const void* object) const noexcept
+        {
+            ILL_ASSERT(object);
+            return object != nullptr && m_object == object;
+        }
+
+        void Clear() noexcept
+        {
+            m_object = nullptr;
+            m_stub = nullptr;
+        }
+
+        void Execute() const
+        {
+            ILL_ASSERT(IsBound());
+            m_stub(m_object, std::forward<Args>(args)...);
+        }
+
+        template<typename T, void (T::*Method)(Args...)>
+        static RawDelegate Create(T* object) noexcept
+        {
+            ILL_ASSERT(object);
+
+            RawDelegate delegate;
+            delegate.m_object = object;
+            delegate.m_stub = [](void* object, Args... args)
+            {
+                T* typedObject = static_cast<T*>(object);
+                (typedObject->Method)(std::forward<Args>(args)...);
+            }
+
+            return delegate;
+        }
+
+        template<typename T, void (T::*Method)(Args...) const>
+        static RawDelegate Create(const T* object) noexcept
+        {
+            ILL_ASSERT(object);
+
+            RawDelegate delegate;
+            delegate.m_object = object;
+            delegate.m_stub = [](void* object, Args... args)
+            {
+                const T* typedObject = static_cast<const T*>(object);
+                (typedObject->Method)(std::forward<Args>(args)...);
+            }
+
+            return delegate;
+        }
+
+        template<typename T, void (*Function)(Args...) const>
+        static RawDelegate CreateStatic() noexcept
+        {
+            RawDelegate delegate;
+            delegate.m_object = nullptr;
+            delegate.m_stub = [](void*, Args... args)
+            {
+                (Function)(std::forward<Args>(args)...);
+            }
+
+            return delegate;
+        }
+
+    private:
+
+        void* m_object;
+        Stub m_stub;
+    };
+}
